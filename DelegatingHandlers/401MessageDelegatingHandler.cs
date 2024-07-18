b9e95f5324fe651d066a1d326589e98922666684
fukicycle <lego.sato.4135@gmail.com>
2024-07-18 14:45:40 +0900

    Add delegating handler.

---
+++ DelegatingHandlers/401MessageDelegatingHandler.cs
@@ -0,0 +1,22 @@
+﻿
+using Attendiia.Authentication;
+
+namespace Attendiia.DelegatingHandlers;
+
+public sealed class _401MessageDelegatingHandler : DelegatingHandler
+{
+    private readonly FirebaseAuthenticationStateProvider _firebaseAuthenticationStateProvider;
+    public _401MessageDelegatingHandler(FirebaseAuthenticationStateProvider firebaseAuthenticationStateProvider)
+    {
+        _firebaseAuthenticationStateProvider = firebaseAuthenticationStateProvider;
+    }
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        HttpResponseMessage httpResponseMessage = await base.SendAsync(request, cancellationToken);
+        if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            await _firebaseAuthenticationStateProvider.NotifySignOut();
+        }
+        return httpResponseMessage;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ using Blazored.LocalStorage;
 using Attendiia;
 using Attendiia.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
+using Attendiia.DelegatingHandlers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 //configuration check
 builder.Services.AddScoped(p =>
@@ -29,7 +30,7 @@ builder.Services.AddScoped(p =>
     }
     return firebaseSettings;
 });
-
+builder.Services.AddScoped<DelegatingHandler, _401MessageDelegatingHandler>();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
@@ -37,5 +38,8 @@ builder.Services.AddScoped<FirebaseAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(
     p => p.GetRequiredService<FirebaseAuthenticationStateProvider>());
 builder.Services.AddScoped<IAuthenticationService, FirebaseAuthenticationService>();
+builder.Services.AddHttpClient("Default", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<_401MessageDelegatingHandler>();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Default"));
 
 await builder.Build().RunAsync();
