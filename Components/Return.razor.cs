8d52e3796cd73dfd3facd0bedc2dc26fe5ffbc58
fukicycle <lego.sato.4135@gmail.com>
2024-07-28 13:30:56 +0900

    GithubPagesにホストする関係上パスの指定が/から始められないように修正

--- Components/Return.razor.cs
+++ Components/Return.razor.cs
@@ -7,6 +7,15 @@ public partial class Return
     [Parameter]
     public string ReturnTo { get; set; } = null!;
 
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        if (ReturnTo.StartsWith("/"))
+        {
+            throw new ArgumentException("Sorry, retrun url must not start with /.");
+        }
+    }
+
     private void OnClick()
     {
         NavigationManager.NavigateTo(ReturnTo);
