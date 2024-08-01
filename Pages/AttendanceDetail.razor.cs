2e0bcd9ef4bd8446607e2bbdaf5065f90a810fe1
fukicycle <lego.sato.4135@gmail.com>
2024-08-01 18:20:36 +0900

    回答入力欄を実装

--- Pages/AttendanceDetail.razor.cs
+++ Pages/AttendanceDetail.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Attendiia.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Attendiia.Pages;
 
@@ -7,4 +9,92 @@ public partial class AttendanceDetail
     [Parameter]
     [SupplyParameterFromQuery(Name = "id")]
     public string Id { get; set; } = string.Empty;
+
+    [CascadingParameter]
+    public Task<AuthenticationState> AuthenticationState { get; set; } = null!;
+    private Attendance? attendance;
+    private bool isLoading = false;
+    private string message = string.Empty;
+    private string uid = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authenticationState = await AuthenticationState;
+        string? uid = authenticationState.User.Identity?.Name;
+        if (string.IsNullOrEmpty(uid))
+        {
+            message = "認証に失敗しました。";
+            NavigationManager.NavigateTo("", true);
+        }
+        else
+        {
+            this.uid = uid;
+        }
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        try
+        {
+            attendance = await AttendanceService.GetAttendanceByIdAsync(Id);
+        }
+        catch (Exception e)
+        {
+            message = e.Message;
+        }
+    }
+
+    private async Task OKButtonOnClick()
+    {
+        try
+        {
+            isLoading = true;
+            await AttendanceResponseService.SubmitResponseAsync(uid, Id, ResponseType.OK);
+            NavigationManager.NavigateTo("");
+        }
+        catch (Exception e)
+        {
+            message = e.Message;
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
+
+    private async Task HoldButtonOnClick()
+    {
+        try
+        {
+            isLoading = true;
+            await AttendanceResponseService.SubmitResponseAsync(uid, Id, ResponseType.HOLD);
+            NavigationManager.NavigateTo("");
+        }
+        catch (Exception e)
+        {
+            message = e.Message;
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
+
+    private async Task NGButtonOnClick()
+    {
+        try
+        {
+            isLoading = true;
+            await AttendanceResponseService.SubmitResponseAsync(uid, Id, ResponseType.NG);
+            NavigationManager.NavigateTo("");
+        }
+        catch (Exception e)
+        {
+            message = e.Message;
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
 }
